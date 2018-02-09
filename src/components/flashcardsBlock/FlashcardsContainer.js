8b66d121f7db8ef7a1c070e6c1aab6557bdbe597
Kevin Yonzon <kyonzon14@gmail.com>
2018-02-08 17:34:14 -0800

    need to fix interpolation and rid of quotation marks for correctAnswerChoice

--- src/components/flashcardsBlock/FlashcardsContainer.js
+++ src/components/flashcardsBlock/FlashcardsContainer.js
@@ -4,7 +4,38 @@ import '../../App.css'
 import Flashcard from './Flashcard'
 
 class FlashcardsContainer extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      questions: []
+    }
+
+  }
+
+  componentDidMount(){
+    fetch('https://qriusity.com/v1/questions?page=2&limit=3')
+      .then(res => res.json())
+      .then(json =>
+          this.setState({
+            questions: json
+          })
+        )
+  }
+
   render(){
+
+    let flashcards = this.state.questions.map((question) => {
+      let correctAnswerChoice = 'option' + `${question.answers}`
+      {debugger}
+
+      //need to fix interpolation and rid of quotation marks
+
+      return(
+        <Flashcard question={question.question} answer={question.correctAnswerChoice}/>
+      )
+    })
+
     return(
       <div className='flashcards-container'>
         <div className='flashcard-header'>
